2a8d2503ed7cc8fd55ca44456e5b22b58c864c27
Marcello <marcel.buehler@uzh.ch>
2018-11-26 14:30:48 +0100

    starte implementation of init

--- src/evaluation/within_evaluation.py
+++ src/evaluation/within_evaluation.py
@@ -8,5 +8,9 @@ class WithinEvaluation(Evaluation):
     Training set: refined images from UnityEyes
     Test set: refined images from UnityEyes
     """
-    def __init__(self, model_eyegaze, model_gan, data_training, data_test):
+    def __init__(self, model_eyegaze, data_test):
         super().__init__()
+        self.model_eyegaze = model_eyegaze
+        self.data_test = data_test
+
+
