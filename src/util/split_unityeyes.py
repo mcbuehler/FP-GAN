d9603555a04c67e1fba38dcad549fac5b104cf0b
Marcello <marcel.buehler@uzh.ch>
2018-11-28 11:49:36 +0100

    split sizes adjusted

--- src/util/split_unityeyes.py
+++ src/util/split_unityeyes.py
@@ -7,7 +7,8 @@ path_train = "../data/UnityEyesTrain/"
 path_val = "../data/UnityEyesVal/"
 path_test = "../data/UnityEyesTest/"
 
-test_size = 200
+test_size = 10000
+validation_size = 10000
 
 files = os.listdir(path_unity)
 ids = [re.sub(r"[^\d]", "", f) for f in files if f.endswith(".jpg")]
@@ -45,11 +46,11 @@ n_duplicates = len(ids) - len(set(ids))
 print("{} Duplicates found".format(n_duplicates))
 
 
-test_ids = create_new_dataset(ids, 20000, path_unity, path_test)
+test_ids = create_new_dataset(ids, test_size, path_unity, path_test)
 
 unused_ids = [id for id in ids if id not in test_ids]
 
-validation_ids = create_new_dataset(unused_ids, 200, path_unity, path_val)
+validation_ids = create_new_dataset(unused_ids, validation_size, path_unity, path_val)
 
 unused_ids = [id for id in unused_ids if id not in validation_ids]
 copy_samples(unused_ids, path_unity, path_train)
