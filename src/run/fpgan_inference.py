c1d93132fad70a03e9fd65c161277b943d56e21b
Marcello <marcel.buehler@uzh.ch>
2018-11-28 10:19:52 +0100

    added comments to line where there is a bug

--- src/run/fpgan_inference.py
+++ src/run/fpgan_inference.py
@@ -8,32 +8,31 @@ python export_graph.py --model pretrained/apple2orange.pb \
 import os
 import tensorflow as tf
 
-from reader import UnityReader
-from unityeyes import UnityEyes
+from datasources.unityeyes import UnityEyes
 from util import utils
 
 FLAGS = tf.flags.FLAGS
 
 tf.flags.DEFINE_string('model', '', 'model path (.pb)')
 tf.flags.DEFINE_string('input', '', 'input path (folder containing images)')
-tf.flags.DEFINE_string('output', 'output_sample.jpg', 'output image path (.jpg)')
+tf.flags.DEFINE_string('output', '', 'output folder')
 
 tf.flags.DEFINE_integer('image_width', 120, 'default: 120')
 tf.flags.DEFINE_integer('image_height', 72, 'default: 72')
 
 
 def create_folder_if_not_exists(path):
-  if not os.path.exists(path):
-    os.mkdir(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
 
 
 def _parse_function(filename):
-  image_data = tf.read_file(filename)
-  input_image = tf.image.decode_jpeg(image_data, channels=3)
-  input_image = tf.image.resize_images(input_image, size=image_size)
-  input_image = utils.convert2float(input_image)
+    image_data = tf.read_file(filename)
+    input_image = tf.image.decode_jpeg(image_data, channels=3)
+    input_image = tf.image.resize_images(input_image, size=image_size)
+    input_image = utils.convert2float(input_image)
 
-  return input_image
+    return input_image
 
 
 def save_images(tensor, path, image_names):
@@ -42,6 +41,7 @@ def save_images(tensor, path, image_names):
         filepath = "{}.jpg".format(os.path.join(path, image_names[i]))
         with open(filepath, 'wb') as f:
             f.write(tensor[i])
+
 
 batch_size = 2
 LIMIT = 34
@@ -53,89 +53,75 @@ output_folder = os.path.join(model_path, output_subfolder)
 create_folder_if_not_exists(output_folder)
 
 
-
-
-# all_images_names = list(filter(lambda x: x.endswith('jpg'), os.listdir(FLAGS.input)))
-# all_images_paths = [os.path.join(FLAGS.input, f) for f in all_images_names]
-# if LIMIT > 0:
-#     all_images_paths = all_images_paths[:LIMIT]
-# print(len(all_images_paths))
-
-
-
-
-
 def inference():
-  graph = tf.Graph()
+    graph = tf.Graph()
 
-  with graph.as_default():
-    # filenames = tf.constant(all_images_paths)
-    #
-    # dataset = tf.data.Dataset.from_tensor_slices(filenames)
-    # dataset = dataset.map(_parse_function).batch(batch_size)
-    # iterator = dataset.make_one_shot_iterator()
+    with graph.as_default():
+        # filenames = tf.constant(all_images_paths)
+        #
+        # dataset = tf.data.Dataset.from_tensor_slices(filenames)
+        # dataset = dataset.map(_parse_function).batch(batch_size)
+        # iterator = dataset.make_one_shot_iterator()
 
-    with tf.gfile.FastGFile(FLAGS.model, 'rb') as model_file:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(model_file.read())
+        with tf.gfile.FastGFile(FLAGS.model, 'rb') as model_file:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(model_file.read())
 
-        tf.import_graph_def(graph_def, name='output')
+            tf.import_graph_def(graph_def, name='output')
 
-        # for op in tf.get_default_graph().get_operations():
-        #     print(str(op.name))
+            # for op in tf.get_default_graph().get_operations():
+            #     print(str(op.name))
 
-        print([n.name for n in tf.get_default_graph().as_graph_def().node])
-        input_tensor = graph.get_tensor_by_name('output/input_image:0')
-        output_tensor = graph.get_tensor_by_name('output/output_image:0')
+            print([n.name for n in tf.get_default_graph().as_graph_def().node])
+            input_tensor = graph.get_tensor_by_name('output/input_image:0')
+            output_tensor = graph.get_tensor_by_name('output/output_image:0')
 
+        with tf.Session(graph=graph) as sess:
+            image_queue = UnityEyes(sess, batch_size, FLAGS.input,
+                                    testing=True, eye_image_shape=image_size,
+                                    data_format="NHWC", shuffle=False)
+            image_queue.create_and_start_threads()
 
-    with tf.Session(graph=graph) as sess:
+            number_of_entries = image_queue.num_entries
+            number_of_batches = int(number_of_entries / batch_size)
 
-        image_queue = UnityEyes(sess, batch_size, FLAGS.input,
-                                testing=True, eye_image_shape=image_size,
-                                data_format="NHWC", shuffle=False)
-        image_queue.create_and_start_threads()
+            print("number of entries: {}".format(number_of_entries))
+            print("batch size: {}".format(batch_size))
+            print("number of batches: {}".format(number_of_batches))
 
-        number_of_entries = image_queue.num_entries
-        number_of_batches = int(number_of_entries/batch_size)
+            for i in range(number_of_batches):
+                print("Step {} / {}".format(i, number_of_batches - 1))
+                try:
+                    eyes = image_queue.output_tensors['clean_eye']
+                    ids = image_queue.output_tensors['image_id']
 
-        print("number of entries: {}".format(number_of_entries))
-        print("batch size: {}".format(batch_size))
-        print("number of batches: {}".format(number_of_batches))
+                    # Why does it freeze here in the last iteration?
+                    # eyes and ids still have two samples in there...
+                    batch_eyes, batch_ids = sess.run([eyes, ids])
+                    image_ids = [id.decode('utf-8') for id in batch_ids]
 
-        counter = 0
-        for i in range(number_of_batches-1):
-            print("Step {} / {}".format(counter, number_of_batches-1))
-            try:
-                eyes = image_queue.output_tensors['clean_eye']
-                ids = image_queue.output_tensors['image_id']
+                    print("Processing image ids:")
+                    print(image_ids)
 
-                batch_eyes, batch_ids = sess.run([eyes, ids])
-                image_ids = [id.decode('utf-8') for id in batch_ids]
+                    generated = sess.run(output_tensor, feed_dict={
+                        input_tensor: batch_eyes
+                    })
+                    save_images(generated, output_folder, image_ids)
+                    print("Saved {} images".format(len(image_ids)))
 
-                print("Processing image ids:")
-                print(image_ids)
+                    if i * batch_size > LIMIT:
+                        print("processed {} images. stopping".format(
+                            i * batch_size))
+                        break
 
-                generated = sess.run(output_tensor, feed_dict={
-                    input_tensor: batch_eyes
-                })
-                save_images(generated, output_folder, image_ids)
-                print("Saved {} images".format(len(image_ids)))
-                counter += 1
-
-                if counter * batch_size > LIMIT:
-                    print("processed {} images. stopping".format(counter * batch_size))
+                except tf.errors.OutOfRangeError:
+                    print("out of range")
                     break
-
-            except tf.errors.OutOfRangeError:
-                break
-
-            # with open(output_path+"/test.jpg", 'wb') as f:
-            #     f.write(generated)
 
 
 def main(unused_argv):
-  inference()
+    inference()
+
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
