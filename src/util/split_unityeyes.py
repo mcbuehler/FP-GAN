1447684690b5b95e01420b41f1a4a62b036a7782
Marcello <marcel.buehler@uzh.ch>
2018-12-07 20:34:21 +0100

    refactoring

--- src/util/split_unityeyes.py
+++ src/util/split_unityeyes.py
@@ -2,16 +2,7 @@ import os
 import re
 from shutil import copyfile
 import numpy as np
-path_unity = "../data/UnityEyes/"
-path_train = "../data/UnityEyesTrain/"
-path_val = "../data/UnityEyesVal/"
-path_test = "../data/UnityEyesTest/"
 
-test_size = 10000
-validation_size = 10000
-
-files = os.listdir(path_unity)
-ids = [re.sub(r"[^\d]", "", f) for f in files if f.endswith(".jpg")]
 
 def create_filepath(folder, id, suffix):
     filename = "{}.{}".format(id, suffix)
@@ -42,16 +33,25 @@ def create_new_dataset(ids, size, from_path, to_path):
     return samples
 
 
-n_duplicates = len(ids) - len(set(ids))
-print("{} Duplicates found".format(n_duplicates))
+if __name__ == "__main__":
+    path_unity = "../data/UnityEyes/"
+    path_train = "../data/UnityEyesTrain/"
+    path_val = "../data/UnityEyesVal/"
+    path_test = "../data/UnityEyesTest/"
 
+    test_size = 10000
+    validation_size = 10000
 
-test_ids = create_new_dataset(ids, test_size, path_unity, path_test)
+    files = os.listdir(path_unity)
+    ids = [re.sub(r"[^\d]", "", f) for f in files if f.endswith(".jpg")]
+    n_duplicates = len(ids) - len(set(ids))
+    print("{} Duplicates found".format(n_duplicates))
 
-unused_ids = [id for id in ids if id not in test_ids]
+    test_ids = create_new_dataset(ids, test_size, path_unity, path_test)
+    unused_ids = [id for id in ids if id not in test_ids]
 
-validation_ids = create_new_dataset(unused_ids, validation_size, path_unity, path_val)
+    validation_ids = create_new_dataset(unused_ids, validation_size, path_unity, path_val)
 
-unused_ids = [id for id in unused_ids if id not in validation_ids]
-copy_samples(unused_ids, path_unity, path_train)
+    unused_ids = [id for id in unused_ids if id not in validation_ids and id not in test_ids]
+    copy_samples(unused_ids, path_unity, path_train)
 
