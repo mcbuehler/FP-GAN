e463ad9433edc90ebd7583a403582ab215fa2c76
Marcello <marcel.buehler@uzh.ch>
2019-01-03 09:49:19 +0100

    improving visualisations and sampling of images

--- src/visualisations/data_loading.py
+++ src/visualisations/data_loading.py
@@ -4,40 +4,66 @@ import ujson
 from scipy import misc
 from matplotlib.pyplot import imshow
 import matplotlib.pyplot as ply
+import numpy as np
 
 
-def get_mpii(path, identifiers):
-    """
+class DataLoader:
+    def __init__(self, path):
+        self.path = path
 
-    :param path:
-    :param identifiers: list of tuples (person_identifier, index)
-    :return:
-    """
-    out = dict()
+    def get_data(self, identifiers=None):
+        raise NotImplementedError()
 
-    with h5py.File(path, 'r') as hf:
+
+class MPIIDataLoader(DataLoader):
+    def sample_identifiers(self, hf):
+        samples = list()
+        for person_identifier in hf:
+            n = hf[person_identifier]['image'].shape[0]
+            index = np.random.randint(0, n)
+            samples.append((person_identifier, index))
+        return samples
+
+    def get_data(self, identifiers=None):
+        """
+
+        :param path:
+        :param identifiers: list of tuples (person_identifier, index)
+        :return:
+        """
+        out = dict()
+
+        with h5py.File(self.path, 'r') as hf:
+            identifiers = self.sample_identifiers(hf) if identifiers is None else identifiers
+            for i, (person_identifier, index) in enumerate(identifiers):
+                out[identifiers[i]] = {
+                    'eye': hf[person_identifier]['image'][index][..., ::-1],
+                    'gaze': hf[person_identifier]['gaze'][index]
+                }
+        return out
+
+
+class RefinedMPIIDataLoader(DataLoader):
+    def sample_identifiers(self):
+        person_identifiers = list(set([f[:3] for f in os.listdir(self.path)]))
+        index = np.random.randint(0, 1000, len(person_identifiers))
+        return [(person_identifiers[i], index[i]) for i in range(len(index))]
+
+    def get_data(self, identifiers=None):
+        def get_file_path(path, person_identifier, index, postfix):
+            return os.path.join(path, "{}_{}.{}".format(person_identifier, index, postfix))
+
+        out = dict()
+        identifiers = self.sample_identifiers() if identifiers is None else identifiers
         for i, (person_identifier, index) in enumerate(identifiers):
+            img = misc.imread(get_file_path(self.path, person_identifier, index, 'jpg'))
+            with open(get_file_path(self.path, person_identifier, index, 'json'), 'r') as f:
+                json_data = ujson.load(f)
             out[identifiers[i]] = {
-                'eye': hf[person_identifier]['image'][index][..., ::-1],
-                'gaze': hf[person_identifier]['gaze'][index]
+                'eye': img,
+                'gaze': json_data['gaze']
             }
-    return out
-
-
-def get_refined_mpii(path, identifiers):
-    def get_file_path(path, person_identifier, index, postfix):
-        return os.path.join(path, "{}_{}.{}".format(person_identifier, index, postfix))
-
-    out = dict()
-    for i, (person_identifier, index) in enumerate(identifiers):
-        img = misc.imread(get_file_path(path, person_identifier, index, 'jpg'))
-        with open(get_file_path(path, person_identifier, index, 'json'), 'r') as f:
-            json_data = ujson.load(f)
-        out[identifiers[i]] = {
-            'eye': img,
-            'gaze': json_data['gaze']
-        }
-    return out
+        return out
 
 
 if __name__ == "__main__":
--- src/visualisations/visualise_translated.py
+++ src/visualisations/visualise_translated.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
-from visualisations.data_loading import get_refined_mpii, get_mpii
-
+from visualisations.data_loading import MPIIDataLoader, RefinedMPIIDataLoader
+import h5py
+import numpy as np
 from util.gaze import draw_gaze
 
 
@@ -8,7 +9,18 @@ class M2UVisualisation:
     def __init__(self):
         self.path_mpii = '../data/MPIIFaceGaze/single-eye-right_zhang.h5'
         self.path_m2u = '../data/refined_MPII2Unity/'
+        self.dl_mpii = MPIIDataLoader(self.path_mpii)
+        self.dl_refined_mpii = RefinedMPIIDataLoader(self.path_m2u)
         self.do_draw_gaze = True
+
+    def sample_identifiers(self):
+        samples = list()
+        with h5py.File(self.path_mpii, 'r') as hf:
+            for person_identifier in hf:
+                n = hf[person_identifier]['image'].shape[0]
+                index = np.random.randint(0, n)
+                samples.append((person_identifier, index))
+        return samples
 
     @staticmethod
     def dg(img, gaze):
@@ -17,15 +29,26 @@ class M2UVisualisation:
             gaze, length=100.0, thickness=2, color=(0, 255, 0),
         )
 
-    def get_data(self, identifiers):
-        mpii_data = get_mpii(self.path_mpii,
-                         identifiers)
-        m2u_data = get_refined_mpii(self.path_m2u, identifiers)
+    def get_data(self, identifiers=None):
+        mpii_data = self.dl_mpii.get_data(identifiers)
+        m2u_data = self.dl_refined_mpii.get_data(identifiers)
         return  mpii_data, m2u_data
 
-    def visualise(self, identifiers):
+    def visualise(self, identifiers=None):
+        if identifiers is None:
+            identifiers = self.sample_identifiers()
         N = len(identifiers)
         fig, axes = plt.subplots(nrows=N, ncols=2, figsize=(20, 20))
+
+        # Axis labels
+        cols = ["MPII (R)", "Refined MPII (S')"]
+        for ax, col in zip(axes[0], cols):
+            ax.set_title(col)
+        for ax, row in zip(axes[:, 0], identifiers):
+            ax.text(s=row.__str__(), x=0.5, y=0.9, size='large')
+        for ax, row in zip(axes[:, 1], identifiers):
+            ax.text(s=row.__str__(), x=0.5, y=0.9, size='large')
+
         mpii_data, m2u_data = self.get_data(identifiers)
         for i, (person_identifier, img_index) in enumerate(identifiers):
             row = i
@@ -40,14 +63,16 @@ class M2UVisualisation:
             axes[row, 0].imshow(img_mpii)
             axes[row, 1].imshow(img_m2u)
 
-        plt.subplots_adjust(wspace=.05, hspace=.05)
-        plt.show()
-        plt.title("MPII vs. refined MPII")
+        plt.subplots_adjust(wspace=.0005, hspace=0.0001, bottom=0, top=0.95)
+
+        # plt.show()
+        plt.savefig('../visualisations/mpii_vs_refined-mpii.png')
 
 
 if __name__ == "__main__":
     m2u_visualisation = M2UVisualisation()
-    identifiers = [
-        ('p00', 0), ('p00', 1), ('p00', 10), ('p00', 11)
-    ]
+    # identifiers = [
+    #     ('p00', 0), ('p00', 1), ('p00', 10), ('p00', 11)
+    # ]
+    identifiers = None
     m2u_visualisation.visualise(identifiers)
