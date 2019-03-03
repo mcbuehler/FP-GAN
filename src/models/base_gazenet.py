066ea67e044942ad880fad8de8d8a9563071c4f1
Marcello <marcel.buehler@uzh.ch>
2019-03-03 12:29:13 +0100

    documentation

--- src/models/base_gazenet.py
+++ src/models/base_gazenet.py
@@ -2,11 +2,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import GraphKeys
 
-import util.ops as ops
 import util.gaze as gaze
-import util.utils as utils
 from util.enum_classes import Mode
-
 
 """
 Original architecture:
@@ -21,19 +18,24 @@ Input (35x55)
 -> FC9600
 -> FC1000
 -> FC3
--> l2 normalisation
+-> l2 normalisation  // ommitted in our case
 -> l2 loss
+
+Hyper-parameters:
 LR = =.001
 batch_size=512
 """
 
 
 class BaseGazeNet:
+    """
+    Base class for eye gaze estimation networks
+    """
+
     def __init__(self,
                  name,
                  batch_size=1,
                  image_size=(78, 120),
-                 use_sigmoid=False,
                  norm='instance',
                  learning_rate=2e-4,
                  beta1=0.9,
@@ -44,29 +46,25 @@ class BaseGazeNet:
         """
         Args:
           name: name
-
           batch_size: integer, batch size
           image_size: list(height, width)
           norm: 'instance' or 'batch'
           learning_rate: float, initial learning rate for Adam
           beta1: float, momentum term of Adam
-          ngf: number of gen filters in first conv layer
-          tf_session: tensorflow session. Needed for loading data sources.
+          beta2: second moment decay rate for Adam
+          Optimiser: optimiser for learning, e.g. tf.train.AdamOptimizer
+          normalise_gaze: Whether the gaze has been normalised
+            from [-pi, pi] to [0,1]
         """
-
         self.name = name
         self.norm = norm
         self.reuse = tf.AUTO_REUSE
-        self.use_sigmoid = use_sigmoid
-
-        self.use_sigmoid = use_sigmoid
         self.batch_size = batch_size
         self.image_size = image_size
         self.learning_rate = learning_rate
         self.beta1 = beta1
         self.beta2 = beta2
         self.Optimiser = Optimiser
-
         self.normalise_gaze = normalise_gaze
 
     def forward(self, input, mode, is_training=True):
@@ -82,10 +80,8 @@ class BaseGazeNet:
     def create_name(self, name, prefix):
         return "{}/{}".format(prefix, name)
 
-    def get_loss(self, iterator, mode, is_training=True, regulariser=None, summary_key=GraphKeys.SUMMARIES):
-        # # summary
-        summary_pref = mode
-
+    def get_loss(self, iterator, mode, is_training=True, regulariser=None,
+                 summary_key=GraphKeys.SUMMARIES):
         input_batch = iterator.get_next()
 
         input_eye = input_batch['eye']
@@ -98,17 +94,21 @@ class BaseGazeNet:
             input_gaze_unnormalised = input_gaze * np.pi
             output_unnormalised = output * np.pi
         else:
+            # There is nothing to do.
             input_gaze_unnormalised = input_gaze
             output_unnormalised = output
 
-        loss_gaze = tf.reduce_mean(tf.squared_difference(output_unnormalised, input_gaze_unnormalised))
+        # We apply standard MSE
+        loss_gaze = tf.reduce_mean(tf.squared_difference(output_unnormalised,
+                                                         input_gaze_unnormalised))
 
         if regulariser is not None:
             # We add a regulariser
-            reg_variables = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+            reg_variables = tf.get_collection(
+                tf.GraphKeys.REGULARIZATION_LOSSES)
             reg_term = tf.contrib.layers.apply_regularization(regulariser,
-                                                          reg_variables)
-            tf.summary.scalar(self.create_name('loss/l2_reg', summary_pref),
+                                                              reg_variables)
+            tf.summary.scalar(self.create_name('loss/l2_reg', mode),
                               reg_term)
 
             loss = loss_gaze + reg_term
@@ -116,60 +116,49 @@ class BaseGazeNet:
             # we do not regularise
             loss = loss_gaze
 
-        error_angular = gaze.tensorflow_angular_error_from_pitchyaw(input_gaze_unnormalised, output_unnormalised)
+        # For easier interpretability we calculate the mean angular error
+        error_angular = gaze.tensorflow_angular_error_from_pitchyaw(
+            input_gaze_unnormalised, output_unnormalised)
 
         # Create summaries
-        tf.summary.image(self.create_name('input/eye', summary_pref), input_eye, max_outputs=1, collections=[summary_key])
+        tf.summary.image(self.create_name('input/eye', mode), input_eye,
+                         max_outputs=1, collections=[summary_key])
 
-        tf.summary.histogram(self.create_name('input/eye', summary_pref), input_eye, collections=[summary_key])
-        tf.summary.histogram(self.create_name('input/gaze', summary_pref), input_gaze_unnormalised, collections=[summary_key])
-        tf.summary.histogram(self.create_name('output/gaze', summary_pref), output_unnormalised, collections=[summary_key])
+        tf.summary.histogram(self.create_name('input/eye', mode), input_eye,
+                             collections=[summary_key])
+        tf.summary.histogram(self.create_name('input/gaze', mode),
+                             input_gaze_unnormalised,
+                             collections=[summary_key])
+        tf.summary.histogram(self.create_name('output/gaze', mode),
+                             output_unnormalised, collections=[summary_key])
 
-        tf.summary.scalar(self.create_name('loss/gaze_mse', summary_pref), loss_gaze, collections=[summary_key])
-        tf.summary.scalar(self.create_name('angular_error', summary_pref), error_angular, collections=[summary_key])
+        tf.summary.scalar(self.create_name('loss/gaze_mse', mode), loss_gaze,
+                          collections=[summary_key])
+        tf.summary.scalar(self.create_name('angular_error', mode),
+                          error_angular, collections=[summary_key])
 
-        return {'gaze_output': output_unnormalised, 'gaze_input': input_gaze_unnormalised, 'error_angular': error_angular}, loss
+        return {'gaze_output': output_unnormalised,
+                'gaze_input': input_gaze_unnormalised,
+                'error_angular': error_angular}, loss
 
     def optimize(self, loss):
-        def make_optimizer(loss, variables=None, name='Adam'):
-            """ Adam optimizer with learning rate 0.0002 for the first 100k
-            steps (~100 epochs)
-                and a linearly decaying rate that goes to zero over
-                the next 100k steps
-            """
-            # why can global step be 0 here? Does it automatically
-            # increase after each optimisation step?
-            global_step = tf.Variable(0, trainable=False)
-            # starter_learning_rate = self.learning_rate
-            # end_learning_rate = 0.0
-            # start_decay_step = int(n_steps / 2)
-            # decay_steps = n_steps - start_decay_step
-            # learning_rate = (
-            #     tf.where(
-            #         tf.greater_equal(global_step, start_decay_step),
-            #         tf.train.polynomial_decay(starter_learning_rate,
-            #                                   global_step - start_decay_step,
-            #                                   decay_steps, end_learning_rate,
-            #                                   power=1.0),
-            #         starter_learning_rate
-            #     )
-            #
-            # )
-            # tf.summary.scalar('learning_rate/{}'.format(name), self.learning_rate)
-
-            learning_step = (
-                # tf.train.GradientDescentOptimizer(self.learning_rate).minimize(loss,
-                #                                                                global_step,
-                #                                                                variables)
-                self.Optimiser(self.learning_rate, beta1=self.beta1, beta2=self.beta2, name=name)
-                    .minimize(loss, global_step=global_step)#,var_list=variables)
-            )
-            return learning_step
-
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(update_ops):
-            return make_optimizer(loss, name='Adam') # self.variables,
+            global_step = tf.Variable(0, trainable=False)
+            learning_step = (
+                self.Optimiser(self.learning_rate, beta1=self.beta1,
+                               beta2=self.beta2, name="Adam")
+                    .minimize(loss, global_step=global_step)
+            )
+        return learning_step
 
     def sample(self, input):
+        """
+        This is used for inference. Feed a placeholder or example image as
+            input and this method will return the output.
+        Args:
+            input: sample input
+        Returns: sample output
+        """
         out = self.forward(input, mode=Mode.SAMPLE, is_training=False)
         return out
